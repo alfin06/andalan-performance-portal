b9579802d793adcd70c3b05c7a9c1c7156af0592
Alfin Rahardja <alfin06@users.noreply.github.com>
2024-01-30 01:35:13 +0700

    Setup Laravel, VueJS, Inertia

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Inertia.js + Vue.js</h1>
+        <button @click="counter++">Counter: {{counter}}</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+let counter = ref(0);
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use Inertia\Inertia;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return Inertia::render('Home');
+// });
+
+Route::middleware('auth')->group(function() {
+    Route::get('/', function() {
+        return Inertia::render('Home');
+    })->middleware('verified')->name('home');
+
+    Route::post('user/logout', [AuthController::class, 'logout'])->name('logout');
+    
+    Route::get('/email/verify', [AuthController::class, 'emailVerification'])->middleware('auth')->name('verification.notice');
+   
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+     
+        return redirect()->route('home')->with([
+            'message' => 'Email verified successfully',
+            'class' => 'alert alert-success'
+        ]);
+    })->middleware('signed')->name('verification.verify');
+    
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+     
+        return redirect()->back()->with([
+            'message' => 'Verification link sent!',
+            'class' => 'alert alert-success'
+        ]);
+    })->middleware('throttle:6,1')->name('verification.send');
+});
+
+Route::middleware('guest')->group(function() {
+    Route::get('user/register', [AuthController::class, 'register'])->name('register');
+    
+    Route::post('user/register', [AuthController::class, 'store'])->name('register');
+    
+    Route::get('user/login', [AuthController::class, 'login'])->name('login');
+    
+    Route::post('user/login', [AuthController::class, 'auth'])->name('login');
 });
